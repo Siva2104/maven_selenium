384e8681b31ed0eb165b271ef9f588ee30e160df
1925566 <1925566@10.146.155.229>
2023-12-28 11:25:36 +0530

    maven

---
+++ src/test/java/maven_project/FirstClass.java
@@ -0,0 +1,14 @@
+package maven_project;
+
+import org.testng.annotations.Test;
+
+public class FirstClass {
+
+@Test	
+	
+	public static void main() {
+		// TODO Auto-generated method stub
+		System.out.println("TestCase Passed - TC0032");
+	}
+
+}
